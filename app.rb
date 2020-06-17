d481777b38870243d949e37a904277bc95a292df
Irene Scott <irenejinscott@gmail.com>
2020-06-17 18:53:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,9 +34,9 @@ class App < Sinatra::Base
     end
 
     get '/:operation/:number1/:number2' do
-            answer = params[:answer]
-            number1 = params[:number1].to_i
-            number2 = params[:number2].to_i
+      operation = params[:operation]
+      number1 = params[:number1].to_i
+      number2 = params[:number2].to_i
 
             case operation
           	when 'add'
