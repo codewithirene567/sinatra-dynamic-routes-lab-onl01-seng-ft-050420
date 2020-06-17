60bc946d5c5a7d3670e637999e7548b8e8c8f31d
Irene Scott <irenejinscott@gmail.com>
2020-06-17 18:53:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -38,16 +38,16 @@ class App < Sinatra::Base
             number1 = params[:number1].to_i
             number2 = params[:number2].to_i
 
-            case answer
-            when 'add'
-              (number1 + number2).to_s
-            when 'subtract'
-               (number1 - number2).to_s
-            when 'multiply'
-              (number1 * number2).to_s
-            when 'divide'
-              (number1 / number2).to_s
-          end
+            case operation
+          	when 'add'
+          		(number1 + number2).to_s
+          	when 'subtract'
+          		(number1 - number2).to_s
+          	when 'multiply'
+          		(number1 * number2).to_s
+          	when 'divide'
+          		(number1 / number2).to_s
+          	end
     end
 
 
