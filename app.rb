6de0b77e06411cde057b0e3f9f0ea39433a681e5
Irene Scott <irenejinscott@gmail.com>
2020-06-17 18:24:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,11 @@ class App < Sinatra::Base
   get '/square/:number' do
     @square_number = params[:number].to_i ** 2
     @square_number.to_s
-    binding.pry
-  end
+    
+     #2 ** 3        #=> 8
+    end
+
+  get '/say/:number/:phrase' do
+    @number_phrase = params[:phrase] * [:number]
+    @number_phrase.to_s
 end
